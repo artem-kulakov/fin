82ee6a6e9c2d2ee856a3fcb12fe4b896018dbaa8
Artem Kulakov <artem.kulakov@gmail.com>
2019-08-08 07:12:32 +0300

    Period scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_181433) do
+ActiveRecord::Schema.define(version: 2019_08_08_041114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "industries", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "periods", force: :cascade do |t|
+    t.datetime "period"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
