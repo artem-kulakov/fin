d970d4ac3f0260e6ea49d12345227acbdd1fad95
Artem Kulakov <artem.kulakov@gmail.com>
2019-08-08 08:32:28 +0300

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,31 @@
+Report.delete_all
+Company.delete_all
 Industry.delete_all
+Period.delete_all
+Standard.delete_all
+Currency.delete_all
+Indicator.delete_all
+
 industry1 = Industry.create( name: 'Telecom' )
 industry2 = Industry.create( name: 'IT' )
 
-Period.delete_all
-Period.create([
-  { year: 2018 },
-  { year: 2017 },
-])
+period1 = Period.create( year: 2018 )
+period2 = Period.create( year: 2017 )
 
-Standard.delete_all
-Standard.create([
-  { name: 'IFRS' },
-  { name: 'US GAAP' },
-  { name: 'Other' }
-])
+standard1 = Standard.create( name: 'IFRS' )
+standard2 = Standard.create( name: 'US GAAP' )
 
-Currency.delete_all
-Currency.create([
-  { name: 'USD' },
-  { name: 'RUB' }
-])
+currency1 = Currency.create( name: 'USD' )
+currency2 = Currency.create( name: 'USD' )
 
-Indicator.delete_all
 Indicator.create([
   { name: 'Revenues' },
   { name: 'Net income' },
   { name: 'Debt' }
 ])
 
-Company.delete_all
-Company.create( name: 'MTS', industry_id: industry1.id )
-Company.create( name: 'Yandex', industry_id: industry2.id )
+company1 = Company.create( name: 'MTS', industry_id: industry1.id )
+company2 = Company.create( name: 'Yandex', industry_id: industry2.id )
+
+Report.create( company_id: company1.id, period_id: period1.id, standard_id: standard1.id, currency_id: currency1.id )
+Report.create( company_id: company2.id, period_id: period2.id, standard_id: standard2.id, currency_id: currency2.id )
