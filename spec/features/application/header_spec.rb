5bc20c12088312ad337068534c2832b7f72b8f85
Artem Kulakov <artem.kulakov@gmail.com>
2019-08-08 08:23:47 +0300

    Header

---
+++ spec/features/application/header_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature 'header' do
+  scenario "test header" do
+    visit(root_path)
+    expect(page).to have_content('FIN')
+    expect(page).to have_content('Reports')
+    expect(page).to have_content('Companies')
+    expect(page).to have_content('Industries')
+  end
+end
