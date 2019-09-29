dba6d88f8572cd6ac0eff5498e0dc8106f74e4ac
Artem Kulakov <artem.kulakov@gmail.com>
2019-09-29 11:33:49 +0300

    Test cancellation of report destroying

--- spec/features/reports/destroy_spec.rb
+++ spec/features/reports/destroy_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature 'destroy report' do
+  scenario 'click Destroy and cancel' do
+    visit(root_path)
+
+    first(".destroy-report").click
+
+    page.driver.browser.switch_to.alert.dismiss
+
+    expect(page).to have_selector('.report-name', count: 2)
+  end
+
   scenario 'destroy report' do
     visit(root_path)
+
     first(".destroy-report").click
+
+    page.driver.browser.switch_to.alert.accept
+
     expect(page).to have_content('Report was successfully destroyed.')
+
+    expect(page).to have_selector('.report-name', count: 1)
   end
 end
