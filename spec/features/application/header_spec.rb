8d4375a0f6e166e559450953ed07daa6c4f93909
Artem Kulakov <artem.kulakov@gmail.com>
2019-10-07 12:42:13 +0300

    Test presence of sign-in/out in header

--- spec/features/application/header_spec.rb
+++ spec/features/application/header_spec.rb
@@ -4,5 +4,6 @@ RSpec.feature 'header' do
   scenario 'test header' do
     visit(root_path)
     expect(page).to have_content('FIN')
+    expect(page).to have_content('Sign in').or(have_content('Sign out'))
   end
 end
